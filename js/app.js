325c106619129c086bf3a0c824695f5eab1cca10
Francesca-Barletta <wallebar@gmail.com>
2024-03-27 15:46:53 +0100

    add input and button to  add list item

--- js/app.js
+++ js/app.js
@@ -1,43 +1,56 @@
 
-  const { createApp } = Vue
+const { createApp } = Vue
 
-  createApp({
+createApp({
     data() {
-      return {
-        message: 'todolist!',
-        todo: [
-            {
-                text:'lavare la macchina',
-                done: false
-            },
-            {
-                text:'fare la spesa',
-                done: false
-            },
-            {
-                text:'portare fuori il cane',
-                done: false
-            },
-            {
-                text:'fare gli esercizi al pc',
-                done: false
-            },
-            {
-                text:'pagare la bolletta del gas',
-                done: false
-            },
-        ],
+        return {
+            message: 'todolist!',
+            userValue: '',
+            todos: [
+                {
+                    text: 'lavare la macchina',
+                    done: false
+                },
+                {
+                    text: 'fare la spesa',
+                    done: false
+                },
+                {
+                    text: 'portare fuori il cane',
+                    done: false
+                },
+                {
+                    text: 'fare gli esercizi al pc',
+                    done: false
+                },
+                {
+                    text: 'pagare la bolletta del gas',
+                    done: false
+                },
+            ],
 
-      }
+        }
     },
     methods: {
         removeListItem(i) {
-                this.todo.splice(i, 1);
+            this.todos.splice(i, 1);
+        },
+        addListItem() {
+            if (this.userValue !== '') {
+                // Creo l'oggetto da inserire
+                let newTodo = {
+                    done: false,
+                    text: '',
+                }
+                // Inserisco nell'oggetto il testo preso da quello inserito dall'utente
+                newTodo.text = this.userValue;
+                // Inserisco l'oggetto nell'array
+                this.todos.push(newTodo);
+                this.userValue = '';
             }
         },
+    },
     mounted() {
-	
-
-	},
-  }).mount('#app');
+    },
+}).mount('#app');
 
