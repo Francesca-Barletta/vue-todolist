5fdf922567f9ad4557d8a020d03829fd98c498c0
Francesca-Barletta <wallebar@gmail.com>
2024-03-27 15:21:41 +0100

    add function remove at click on x

--- js/app.js
+++ js/app.js
@@ -4,7 +4,40 @@
   createApp({
     data() {
       return {
-        message: 'todolist!'
+        message: 'todolist!',
+        todo: [
+            {
+                text:'lavare la macchina',
+                done: false
+            },
+            {
+                text:'fare la spesa',
+                done: false
+            },
+            {
+                text:'portare fuori il cane',
+                done: false
+            },
+            {
+                text:'fare gli esercizi al pc',
+                done: false
+            },
+            {
+                text:'pagare la bolletta del gas',
+                done: false
+            },
+        ],
+
       }
-    }
-  }).mount('#app')
+    },
+    methods: {
+        removeListItem(i) {
+                this.todo.splice(i, 1);
+            }
+        },
+    mounted() {
+	
+
+	},
+  }).mount('#app');
+
